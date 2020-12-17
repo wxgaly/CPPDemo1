09340fcbbd16a1108313087dba27185ca181ed2c
wangxuguang <962831870@qq.com>
2020-12-17 14:32:26 +0800

    add: 买卖股票的最佳时机含手续费   +1

---
+++ leetcode/medium/BestTimetoBuyandSellStockwithTransactionFee.hpp
@@ -0,0 +1,49 @@
+/*
+ * @Author: gary.wang
+ * @Date: 2020-12-17 14:29:40
+ * @Last Modified by: gary.wang
+ * @Last Modified time: 2020-12-17 14:31:32
+ *
+
+    Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i;
+    and a non-negative integer fee representing a transaction fee.
+
+    You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+     You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+
+    Return the maximum profit you can make.
+
+    Example 1:
+    Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
+    Output: 8
+    Explanation: The maximum profit can be achieved by:
+    Buying at prices[0] = 1
+    Selling at prices[3] = 8
+    Buying at prices[4] = 4
+    Selling at prices[5] = 9
+    The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
+ *
+ *
+ */
+#include "../ITest.h"
+
+class BestTimetoBuyandSellStockwithTransactionFee : public ITest {
+  public:
+    //题解，贪心，https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-han-sh-rzlz/
+    int maxProfit(vector<int> &prices, int fee) {
+        int n = prices.size();
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+    void test() {}
+};
