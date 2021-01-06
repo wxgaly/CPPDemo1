9fc720d3b6eaac2e97a12af6615868c0552dc736
wangxuguang <962831870@qq.com>
2021-01-06 14:55:52 +0800

    add: 除法求值   +1  打卡

---
+++ leetcode/medium/EvaluateDivision.hpp
@@ -0,0 +1,90 @@
+/*
+ * @Author: gary.wang
+ * @Date: 2021-01-06 14:31:02
+ * @Last Modified by: gary.wang
+ * @Last Modified time: 2021-01-06 14:55:04
+ *
+   
+    You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the
+    equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+    You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+    Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+    Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+     
+
+    Example 1:
+
+    Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+    Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+    Explanation:
+    Given: a / b = 2.0, b / c = 3.0
+    queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+    return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+
+
+ *
+ */
+#include "../ITest.h"
+
+class EvaluateDivision : public ITest {
+  public:
+    //跪下了，并查集，根本就不会，就是打卡，，，
+    int findf(vector<int> &f, vector<double> &w, int x) {
+        if (f[x] != x) {
+            int father = findf(f, w, f[x]);
+            w[x] = w[x] * w[f[x]];
+            f[x] = father;
+        }
+        return f[x];
+    }
+
+    void merge(vector<int> &f, vector<double> &w, int x, int y, double val) {
+        int fx = findf(f, w, x);
+        int fy = findf(f, w, y);
+        f[fx] = fy;
+        w[fx] = val * w[y] / w[x];
+    }
+
+    vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &values, vector<vector<string>> &queries) {
+        int nvars = 0;
+        unordered_map<string, int> variables;
+
+        int n = equations.size();
+        for (int i = 0; i < n; i++) {
+            if (variables.find(equations[i][0]) == variables.end()) {
+                variables[equations[i][0]] = nvars++;
+            }
+            if (variables.find(equations[i][1]) == variables.end()) {
+                variables[equations[i][1]] = nvars++;
+            }
+        }
+        vector<int> f(nvars);
+        vector<double> w(nvars, 1.0);
+        for (int i = 0; i < nvars; i++) {
+            f[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int va = variables[equations[i][0]], vb = variables[equations[i][1]];
+            merge(f, w, va, vb, values[i]);
+        }
+        vector<double> ret;
+        for (const auto &q : queries) {
+            double result = -1.0;
+            if (variables.find(q[0]) != variables.end() && variables.find(q[1]) != variables.end()) {
+                int ia = variables[q[0]], ib = variables[q[1]];
+                int fa = findf(f, w, ia), fb = findf(f, w, ib);
+                if (fa == fb) {
+                    result = w[ia] / w[ib];
+                }
+            }
+            ret.push_back(result);
+        }
+        return ret;
+    }
+    void test() {}
+};
