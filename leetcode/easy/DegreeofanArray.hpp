7d16f6691824f09b09c022469e6f6d81d0f64141
wangxuguang <962831870@qq.com>
2021-02-20 14:44:22 +0800

    add: 数组的度  +1

---
+++ leetcode/easy/DegreeofanArray.hpp
@@ -0,0 +1,36 @@
+/*
+ * @Author: gary.wang
+ * @Date: 2021-02-20 14:43:03
+ * @Last Modified by: gary.wang
+ * @Last Modified time: 2021-02-20 14:43:52
+ */
+#include "../ITest.h"
+
+class DegreeofanArray : public ITest {
+  public:
+    int findShortestSubArray(vector<int> &nums) {
+        unordered_map<int, vector<int>> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (mp.count(nums[i])) {
+                mp[nums[i]][0]++;
+                mp[nums[i]][2] = i;
+            } else {
+                mp[nums[i]] = {1, i, i};
+            }
+        }
+        int maxNum = 0, minLen = 0;
+        for (auto &[_, vec] : mp) {
+            if (maxNum < vec[0]) {
+                maxNum = vec[0];
+                minLen = vec[2] - vec[1] + 1;
+            } else if (maxNum == vec[0]) {
+                if (minLen > vec[2] - vec[1] + 1) {
+                    minLen = vec[2] - vec[1] + 1;
+                }
+            }
+        }
+        return minLen;
+    }
+    void test() {}
+};
